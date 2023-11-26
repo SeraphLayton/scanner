ef9740e110145f3849b3fb7620097159ea80b1ff
Layton <98146885+SeraphLayton@users.noreply.github.com>
2023-11-26 21:38:44 +0100

    Update scanner.py
    added save output to file feature

--- scanner.py
+++ scanner.py
@@ -12,6 +12,7 @@ banner = """
      ┛ 
 """
 
+
 """
 This script can perform an automated nmap scan for single domains and find other subdomains. I use this for hackthebox challenges.
 It focuses on flexibility and customization as it is not faster than nmap itself. Future implementations will come along.
@@ -24,6 +25,8 @@ Year: 2023
 """
 
 
+
+
 def load_wordlist(wordlist_path):
     try:
         with open(wordlist_path, 'r') as file:
@@ -33,7 +36,7 @@ def load_wordlist(wordlist_path):
         print(Fore.RED + f"Wordlist file not found at: {wordlist_path}" + Style.RESET_ALL)
         return []
 
-def find_subdomains(target, wordlist_path):
+def find_subdomains(target, wordlist_path, save_to_file):
     try:
         subdomains = load_wordlist(wordlist_path)
 
@@ -51,13 +54,23 @@ def find_subdomains(target, wordlist_path):
             except dns.resolver.NoAnswer:
                 print(Fore.RED + f"No A records found for {full_domain}" + Style.RESET_ALL)
 
+        if save_to_file:
+            filename = f"results_{target}_dns.txt"
+            with open(filename, 'w') as file:
+                sys.stdout = file
+                for subdomain in found_subdomains:
+                    print(f"Found Subdomain: {subdomain}")
+            sys.stdout = sys.__stdout__
+
         return found_subdomains
     except Exception as e:
         print(Fore.RED + f"An error occurred during subdomain enumeration: {e}" + Style.RESET_ALL)
         return []
 
-def nmap_scan(target, ports):
+def nmap_scan(target, ports, save_to_file):
     try:
+        output_data = []
+
         for port in ports:
             print(f"Scanning port: {port}")  # (<- more verbose)
             nm = nmap.PortScanner()
@@ -82,15 +95,21 @@ def nmap_scan(target, ports):
                                     if cpe:
                                         service_info += f" CPE: {cpe}"
                                     print(Fore.YELLOW + f"{service_info}" + Style.RESET_ALL)
+                                    output_data.append(f"{scanned_port} - State: {state}, {service_info}")
                                 if scripts:
-                                    print(Fore.YELLOW + "Scripts:")
+                                    output_data.append("Scripts:")
                                     for script_name, script_output in scripts.items():
-                                        print(Fore.YELLOW + f"  {script_name}:")
-                                        print(Fore.YELLOW + f"    {script_output}" + Style.RESET_ALL)
-                                print()
-                                print(f" ---------------------------------------------------------------------------------- ")
+                                        output_data.append(f"  {script_name}:")
+                                        output_data.append(f"    {script_output}")
+
+        if save_to_file:
+            filename = f"results_{target}_ports.txt"
+            with open(filename, 'w') as file:
+                for line in output_data:
+                    file.write(line + '\n')
+
     except Exception as e:
-            print(Fore.RED + f"An error occurred during port scanning: {e}" + Style.RESET_ALL)
+        print(Fore.RED + f"An error occurred during port scanning: {e}" + Style.RESET_ALL)
 
 def parse_ports(port_arg):
     if '-' in port_arg:
@@ -115,7 +134,7 @@ def get_user_choice():
     print("2. Port Scan" + Style.RESET_ALL)
     choice = input("Enter your choice (1/2): ")
     return choice
-    
+
 def get_user_input_dns():
     try:
         target = input("Enter the target IP/domain: ")
@@ -134,9 +153,13 @@ def get_user_input_port():
         print(Fore.RED + "\nUser interrupted the input process." + Style.RESET_ALL)
         return None, None
 
-def display_usage():
-    print(banner)
-    print(Fore.MAGENTA + "Something went wrong")
+def ask_to_save():
+    while True:
+        save_option = input("Do you want to save the results to a file? (yes/no): ").lower()
+        if save_option in ['yes', 'no']:
+            return True if save_option == 'yes' else False
+        else:
+            print(Fore.RED + "Please enter 'yes' or 'no'." + Style.RESET_ALL)
 
 try:
     choice = get_user_choice()
@@ -144,19 +167,17 @@ try:
     if choice == '1':
         target, wordlist_path = get_user_input_dns()
         if target is not None and wordlist_path is not None:
-            found_subdomains = find_subdomains(target, wordlist_path)
+            save_to_file = ask_to_save()
+            found_subdomains = find_subdomains(target, wordlist_path, save_to_file)
     elif choice == '2':
         target, port_input = get_user_input_port()
         if target is not None and port_input is not None:
+            save_to_file = ask_to_save()
             ports = parse_ports(port_input)
-            nmap_scan(target, ports)
+            nmap_scan(target, ports, save_to_file)
     else:
         print(Fore.RED + "Invalid choice! Please select either '1' or '2'." + Style.RESET_ALL)
-        display_usage()
-
 except ValueError as ve:
     print(Fore.RED + f"Error: {ve}" + Style.RESET_ALL)
-    display_usage()
 except Exception as e:
     print(Fore.RED + f"An error occurred: {e}" + Style.RESET_ALL)
-    display_usage()
