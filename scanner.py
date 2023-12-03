ec1dda6de4feb8b982b5bf2eb8090389d11bbb6e
Layton <98146885+SeraphLayton@users.noreply.github.com>
2023-12-03 20:15:49 +0100

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -15,8 +15,8 @@ banner = """
 
 
 """
-This script can perform an automated nmap scan for single domains and find other subdomains. I use this for hackthebox challenges.
-It focuses on flexibility and customization as it is not faster than nmap itself. Future implementations will come along.
+This script can perform an automated nmap scan for single domains, find other subdomains or vhosts. I use this for hackthebox challenges. 
+It focuses on flexibility and customization as it is not faster than other tools (yet). Future implementations will come along. 
 The goal is to create a good enumeration tool for hackthebox challenges.
 
 Created by: Layton
